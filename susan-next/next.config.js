77823befbb1689e64eb66390b0cc26917152f8b8
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-03 22:05:31 +0900

    format

--- susan-next/next.config.js
+++ susan-next/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	env: {
-		NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
-	},
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
+  },
 };
 
 module.exports = nextConfig;
--- susan-next/postcss.config.js
+++ susan-next/postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  plugins: {  // 利用するプラグインを設定
+  plugins: {
+    // 利用するプラグインを設定
     tailwindcss: {}, // tailwindCSSの利用プラグイン
     autoprefixer: {}, // ベンダープレフィックス自動生成プラグイン
     cssnano: {}, // ビルドファイルを圧縮プラグイン
