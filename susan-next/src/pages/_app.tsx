9268c8a8f92cf7807f443d81173b472c46a444ed
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2022-06-06 17:15:58 +0900

    remove: 😇 reduxなんてなかった

--- susan-next/src/pages/_app.tsx
+++ susan-next/src/pages/_app.tsx
@@ -1,49 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-//import type { Liff } from "@line/liff";
-import { useEffect } from "react";
-import { Provider, useSelector, useDispatch } from "react-redux";
-import { store, AppDispatch, RootState } from "store";
-import { setLiffData } from "store/user";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	const dispatch: AppDispatch = useDispatch();
-	const user = useSelector((state: RootState) => state.user);
-
-	/* useEffect(() => {
-		if (typeof window == "undefined") return;
-		// to avoid `window is not defined` error
-		import("@line/liff")
-			.then((liff) => liff.default)
-			.then((liff) => {
-				liff
-					.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
-					.then(() => {
-						if (!liff.isInClient() && !liff.isLoggedIn()) {
-							liff.login();
-						}
-						const userData = liff.getDecodedIDToken();
-						dispatch(
-							setLiffData({
-								uid: userData?.sub,
-								//name: userData?.name,
-								//picture: userData?.picture,
-								isInClient: liff.isInClient(),
-								isLoggedIn: liff.isLoggedIn(),
-								token: liff.getAccessToken(),
-							})
-						);
-					})
-					.catch((error: Error) => {
-						console.error(error);
-					});
-			});
-	}, []); */
 
 	return (
-		<Provider store={store}>
-			<Component {...pageProps} />
-		</Provider>
+		<Component {...pageProps} />
 	);
 };
 
--- susan-next/src/pages/index.tsx
+++ susan-next/src/pages/index.tsx
@@ -1,16 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useSelector } from "react-redux";
-import { RootState } from "store";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-	const user = useSelector((state: RootState) => state.user);
-
-	const handleConfirm = () => {
-		console.log(user);
-	};
-
 	return (
 		<div>
 			<Head>
@@ -21,15 +13,6 @@ const Home: NextPage = () => {
 
 			<main className={styles.main}>
 				<h1>create-liff-app</h1>
-				{user.liff.isLoggedIn ? (
-					<p>{user.liff.uid}</p>
-				) : (
-					<p> you're not logged in. </p>
-				)}
-				<h2>redux test</h2>
-				<button type='button' onClick={handleConfirm}>
-					確認
-				</button>
 			</main>
 		</div>
 	);
