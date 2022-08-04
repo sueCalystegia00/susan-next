fdef88bd38adb31382139e9c87674fb3cb2028e3
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-08-04 11:33:51 +0900

    fix: 環境変数管理

--- front/next.config.js
+++ front/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
-  },
-  distDir: 'build',
-  optimizeFonts: true,
+	reactStrictMode: true,
+	env: {
+		LIFF_ID: process.env.LIFF_ID,
+	},
+	distDir: "build",
+	optimizeFonts: true,
 };
 
 module.exports = nextConfig;
