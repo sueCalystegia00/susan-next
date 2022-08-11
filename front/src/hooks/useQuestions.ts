d5cab7c0bb0c9c1af7c0d8a4dbd248e69fd0277c
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-08-11 14:07:15 +0900

    update: スクロール時にデータを追加取得

--- front/src/hooks/useQuestions.ts
+++ front/src/hooks/useQuestions.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { Question } from "@/types/models";
 
 interface Questions {
@@ -10,7 +10,8 @@ interface Questions {
  * DBから質疑応答情報を30件ずつ取得する
  */
 const useQuestionsData = () => {
-	const [questions, setQuestions] = useState(<Questions>{});
+	const [questions, setQuestions] = useState<Questions>({});
+	const [isHasMore, setIsHasMore] = useState(true);
 
 	let dataKeys = Object.keys(questions);
 	const startIndex =
@@ -18,25 +19,23 @@ const useQuestionsData = () => {
 			? 0
 			: dataKeys.reverse()[((dataKeys.length / 30) | 0) * 30 - 1]; //(dataKeys.length/30 | 0)は少数以下切り捨ての除算(ビット演算子利用，Math.floorより気持ち速いらしい)
 
-	useEffect(() => {
-		const getQuestionsData = async () => {
-			await axios
-				.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v2/questions/list`, {
-					params: { startIndex: startIndex },
-				})
-				.then((response: AxiosResponse<Questions>) => {
-					const { data } = response;
-					setQuestions(data);
-				})
-				.catch((error) => {
-					alert("サーバーでエラーが発生しました．");
-					console.error(error);
-				});
-		};
-		getQuestionsData();
-	}, []);
+	const getQuestionsDataHandler = async () => {
+		await axios
+			.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v2/questions/list`, {
+				params: { startIndex: startIndex },
+			})
+			.then((response: AxiosResponse<Questions>) => {
+				const { data } = response;
+				setQuestions({ ...questions, ...data });
+				Object.keys(data).length < 30 && setIsHasMore(false);
+			})
+			.catch((error: AxiosError) => {
+				alert("サーバーでエラーが発生しました．");
+				console.error(error);
+			});
+	};
 
-	return questions;
+	return { questions, isHasMore, getQuestionsDataHandler };
 };
 
 export default useQuestionsData;
--- front/src/pages/questionsList.tsx
+++ front/src/pages/questionsList.tsx
@@ -1,24 +1,40 @@
 import DefaultLayout from "@/layouts/Default";
 import QuestionCard from "@/components/QuestionCard";
-import useQuestionsData from "@/hooks/useQuestions";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useEffect } from "react";
+import useQuestions from "@/hooks/useQuestions";
 
 const QuestionListPage = () => {
-	const questionsData = useQuestionsData();
+	const { questions, isHasMore, getQuestionsDataHandler } = useQuestions();
+
+	useEffect(() => {
+		getQuestionsDataHandler();
+	}, []);
 
 	return (
 		<DefaultLayout>
-			<ul>
-				{Object.keys(questionsData).map((key) => (
-					<QuestionCard
-						key={key}
-						id={Number(key)} // questionId
-						timestamp={questionsData[key].timestamp} // timestamp
-						answerStatus={false} // answerStatus
-						lectureNumber={1} // lectureNumber
-						questionText={questionsData[key].QuestionText} // questionText
-					/>
-				))}
-			</ul>
+			<InfiniteScroll
+				dataLength={Object.keys(questions).length} //現在のデータの長さ
+				next={getQuestionsDataHandler} // スクロール位置を監視してコールバック（次のデータを読み込ませる）
+				hasMore={isHasMore} // さらにスクロールするかどうか（ある一定数のデータ数に達したらfalseを返すことで無限スクロールを回避）
+				loader={null} // ローディング中のコンポーネント
+				//height={420} // 高さ（なくても良い）
+			>
+				<ul>
+					{Object.keys(questions)
+						.reverse()
+						.map((key) => (
+							<QuestionCard
+								key={key}
+								id={Number(key)} // questionId
+								timestamp={questions[key].timestamp} // timestamp
+								answerStatus={false} // answerStatus
+								lectureNumber={1} // lectureNumber
+								questionText={questions[key].QuestionText} // questionText
+							/>
+						))}
+				</ul>
+			</InfiniteScroll>
 		</DefaultLayout>
 	);
 };
