4d1cc1a30ccad05abe115b8fec8f1036594f9e18
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-08-21 21:22:23 +0900

    add: 回答・スレッド表示切り替えボタン

--- front/src/pages/question/[questionId].tsx
+++ front/src/pages/question/[questionId].tsx
@@ -4,6 +4,7 @@ import useQuestions from "@/hooks/useQuestions";
 import DefaultLayout from "@/layouts/Default";
 import { Question } from "@/types/models";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const QuestionDetailsPage = () => {
 	const { questions, getOneQuestionDataHandler } = useQuestions();
@@ -14,13 +15,41 @@ const QuestionDetailsPage = () => {
 		questions[Number(questionId)] ||
 		getOneQuestionDataHandler(Number(questionId));
 
+	const [canViewAnswer, setCanViewAnswer] = useState(!!question.AnswerText);
+
 	return (
 		<DefaultLayout>
 			<QuestionTextDisplay
 				questionText={question.QuestionText}
 				lectureNumber={1}
 			/>
-			<AnswerTextDisplay answerText={question.AnswerText} />
+			{canViewAnswer ? (
+				<>
+					<AnswerTextDisplay answerText={question.AnswerText} />
+					<button
+						className='w-full h-6 text-sm text-align-center border-none bg-gray-200 dark:text-slate-50 dark:bg-slate-500'
+						onClick={() => setCanViewAnswer(false)}
+					>
+						チャットのやり取りを表示
+					</button>
+				</>
+			) : (
+				<>
+					<button
+						className='w-full h-6 text-sm text-align-center border-none bg-gray-200 dark:text-slate-50 dark:bg-slate-500'
+						onClick={() => setCanViewAnswer(true)}
+					>
+						回答のみ表示
+					</button>
+					<div> no answer </div>
+					<button
+						className='w-full h-6 text-sm text-align-center border-none bg-gray-200 dark:text-slate-50 dark:bg-slate-500'
+						onClick={() => setCanViewAnswer(true)}
+					>
+						回答のみ表示
+					</button>
+				</>
+			)}
 		</DefaultLayout>
 	);
 };
