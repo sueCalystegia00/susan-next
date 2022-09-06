0a2255452e2897f0f8cfb77e21c486d5dcb4217c
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-09-06 21:02:01 +0900

    test: 再度detectIntent呼び出しをコメントアウト

--- front/src/pages/api/v1/line/callback/handlers/handleText.ts
+++ front/src/pages/api/v1/line/callback/handlers/handleText.ts
@@ -28,7 +28,7 @@ const handleText = async (
 			contexts[0]
 		);
 
-		const nlpResult = await detectIntent(event.text, contexts);
+		//const nlpResult = await detectIntent(event.text, contexts);
 
 		// create a echoing text message
 		const echo: TextMessage[] = [
@@ -36,10 +36,10 @@ const handleText = async (
 				type: "text",
 				text: `${JSON.stringify(event)}`,
 			},
-			{
+			/* {
 				type: "text",
 				text: `${nlpResult.queryResult?.action || "no action"}`,
-			},
+			}, */
 		];
 		// use reply API
 		client.replyMessage(replyToken, echo);
