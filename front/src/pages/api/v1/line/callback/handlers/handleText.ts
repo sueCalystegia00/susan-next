fc1595c9d9535813c8015db989eacf1bbc00c945
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-09-06 14:49:24 +0900

    test comment out

--- front/src/pages/api/v1/line/callback/handlers/handleText.ts
+++ front/src/pages/api/v1/line/callback/handlers/handleText.ts
@@ -30,7 +30,7 @@ const handleText = async (
 			contexts[0]
 		);
 
-		const nlpResult = await detectIntent(event.text, contexts);
+		//const nlpResult = await detectIntent(event.text, contexts);
 
 		// create a echoing text message
 		const echo: TextMessage[] = [
@@ -42,10 +42,10 @@ const handleText = async (
 				type: "text",
 				text: `${JSON.stringify(contexts)}`,
 			},
-			{
+			/* {
 				type: "text",
 				text: `${JSON.stringify(nlpResult)}`,
-			},
+			}, */
 		];
 		// use reply API
 		await client.replyMessage(replyToken, echo);
