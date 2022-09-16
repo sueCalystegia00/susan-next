6bf8e52af63d4cec9ca5528e96b73da51aeb39e4
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-09-16 21:59:44 +0900

    change: head

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -6,17 +6,31 @@ import Authenticated from "@/components/Authenticated";
 import AuthProvider from "@/contexts/AuthContext";
 import useScrollTop from "@/hooks/useScrollTop";
 import DefaultLayout from "@/layouts/Default";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
 	useScrollTop();
 
 	return (
-		<AuthProvider>
-			<Authenticated />
-			<DefaultLayout>
-				<Component {...pageProps} key={router.asPath} />
-			</DefaultLayout>
-		</AuthProvider>
+		<>
+			<Head>
+				<title>SUSAN Next</title>
+				<meta
+					name='description'
+					content='質問対応チャットボットSUSANと連携するLIFFアプリケーションです'
+				/>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta name='robots' content='noindex,nofollow' />
+			</Head>
+
+			<AuthProvider>
+				<Authenticated />
+				<DefaultLayout>
+					<Component {...pageProps} key={router.asPath} />
+				</DefaultLayout>
+			</AuthProvider>
+		</>
 	);
 };
 
