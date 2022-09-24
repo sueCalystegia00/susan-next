a11726e8615da9a89b7f79ecd9ad25e5cba1a832
sueCalystegia00 <52482280+susanbasket0410@users.noreply.github.com>
2022-09-24 13:43:58 +0900

    fix: 微修正

--- front/src/pages/questionsList/index.tsx
+++ front/src/pages/questionsList/index.tsx
@@ -3,7 +3,6 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import useQuestions from "@/hooks/useQuestions";
 import DefaultLayout from "@/layouts/Default";
 import Loader from "@/components/Loader";
-import { relative } from "path";
 
 /**
  * @returns 質問一覧ページ
@@ -18,7 +17,7 @@ const QuestionsListPage = () => {
 					dataLength={questions.length} //現在のデータの長さ
 					next={getQuestionsDataHandler} // スクロール位置を監視してコールバック（次のデータを読み込ませる）
 					hasMore={questions.slice(-1)[0]?.index != 1 || false} // さらにスクロールするかどうか（index:1の質問があればfalseを返すことで無限スクロールを回避）
-					loader={Loader({ position: relative })} // ローディング中のコンポーネント
+					loader={Loader()} // ローディング中のコンポーネント
 					//height={420} // 高さ（なくても良い）
 				>
 					<ul className='flex flex-col gap-3 p-2'>
