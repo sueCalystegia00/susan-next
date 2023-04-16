06bd6fe2cb4a8581c118f88fed412c31798b9409
sueCalystegia00 <susan.basket0410@gmail.com>
2023-04-16 13:39:13 +0900

    update: ngrok install

--- back/api/v2/index.php
+++ back/api/v2/index.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors',1);
 
-header("Access-Control-Allow-Origin: http://localhost:3000"); //開発用
+header("Access-Control-Allow-Origin: *"); //開発用
 
 include(dirname( __FILE__)."/../database.php");
 
